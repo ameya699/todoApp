a843179f6adbb1c3f52dbe24de7899edc6ac5687
ameya699 <awatadeameya68@gmail.com>
2023-10-02 17:59:41 +0530

    Initial commit - added basic UI and new task creation

---
+++ src/Components/MainPage.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "../Styles/MainPage.css";
+import newtask from "../Images/newtask.png";
+const MainPage = () => {
+  const [currvalue, setcurrvalue] = useState("");
+  const handleChange = (e) => {
+    setcurrvalue(e.target.value);
+  };
+  return (
+    <div className="mainpagelayout">
+      <form
+        onSubmit={() => alert("submitted")}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          columnGap: "2rem",
+          flexWrap: "wrap",
+          rowGap: "2rem",
+        }}
+      >
+        <input
+          type="text"
+          className="create-tasks"
+          value={currvalue}
+          onChange={handleChange}
+          placeholder="New Task"
+        />
+        <button type="submit" style={{ outline: "none", borderRadius: "15px" }}>
+          Add Task
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../Styles/Navbar.css";
+import gitlogo from "../Images/gitlogo.png";
+import linkedInlogo from "../Images/linkdein.png";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 style={{ textAlign: "center" }}>To Do's</h1>
+      <div style={{ display: "flex", columnGap: "0.2rem" }}>
+        <a
+          href="https://www.linkedin.com/in/ameyaawatade/"
+          target="_blank"
+          style={{ alignSelf: "center" }}
+        >
+          <img
+            src={linkedInlogo}
+            style={{
+              height: "40px",
+              width: "40px",
+              backgroundBlendMode: "color-dodge",
+            }}
+          />
+        </a>
+        <a
+          href="https://github.com/ameya699/todoApp"
+          target="_blank"
+          style={{ alignSelf: "center" }}
+        >
+          <img src={gitlogo} style={{ height: "50px" }} />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
